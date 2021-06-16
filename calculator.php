fd1fdb4c8e5aedb80719a0de31b7bc432540b556
Sisoxx <84010734+Sisoxx@users.noreply.github.com>
2021-06-16 12:04:15 +0200

    ll

--- calculator.php
+++ calculator.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>Calculateur en ligne</title>
+	<title>Simulator</title>
 	<link rel="stylesheet" href="style/style.css">
+	<link rel="stylesheet" href="style/style3.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> -->
 </head>
