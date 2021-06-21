5c109187cdc949962d8ae0fa5f1d12dae7e1c518
Sisoxx <84010734+Sisoxx@users.noreply.github.com>
2021-06-21 21:47:37 +0200

    Download page

---
+++ download.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Download</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  </head>
+
+  <header>
+    <?php include("layout/header.html"); ?>
+  </header>
+
+  <body>
+    <section id="contact">
+      <h1>Download files</h1><br>
+      <p>Here you will find some .stl files to download, they represent different phantoms that we developped.</p><br>
+      <a href="image/camera1.jpg" donloawd="camera1">Donloawd File</a>
+    </section>
+
+  <footer>
+    <?php include("layout/footer.html"); ?>
+  </footer>
+  </body>
+</html>
