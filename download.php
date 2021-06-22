b1fc0693667e3d4287eda895a1c6b41f1e9fdfde
Ermis Cherry-Pellat <ermis.cherrypellat@gmail.com>
2021-06-22 10:31:22 +0200

    jsp

--- download.php
+++ download.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Contact Us</title>
+    <title>Download</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
   </head>
@@ -13,16 +13,11 @@
 
   <body>
     <section id="contact">
-      <h1>Contact Us</h1>
-    <div>
-      <form method="POST" action="manager/contact.php" id="formulaire">
-        <input name="nom" placeholder="Name" required>
-        <input name="email" placeholder="E-mail" required> <br>
-        <textarea name="message" id="textearea" placeholder="Your message here ..." required></textarea><br>
-        <button>To Send</button>
-      </form>
-    </div>
+      <h1>Download files</h1><br>
+      <p>Here you will find some .stl files to download, they represent different phantoms that we developped.</p><br>
+      <a href="image/camera1.jpg" download="camera1">Donloawd File</a>
     </section>
+
   <footer>
     <?php include("layout/footer.html"); ?>
   </footer>
