d08ad437a49bd42ef0cd727bc3b5cb5f41ca7736
Sisoxx <84010734+Sisoxx@users.noreply.github.com>
2021-07-21 15:24:16 +0200

    k

--- publications.php
+++ publications.php
@@ -15,13 +15,13 @@
     <div id="publications">
     <h1>Publications</h1>
     <p>Here you will find soon some publications, that will help you to understand how our simulator works.</p>
-    <ul>
-      <li>Joachimowicz, N.; Conessa, C.; Henriksson, T.; Duchêne, B. <a href="https://ieeexplore.ieee.org/document/6849465" target="_blank">"Breast phantoms for microwave
+    <ol>
+      <li>N. Joachimowicz, C. Conessa, T. Henriksson, B. Duchêne <a href="https://ieeexplore.ieee.org/document/6849465" target="_blank">"Breast phantoms for microwave
         imaging"</a>. IEEE Antennas Wireless Propag. Lett. 2014, 13, 1333–1336, DOI:10.1109/LAWP.2014.2336373.</li><br><br>
       <li>N. Joachimowicz, B. Duchêne, C. Conessa, and O. Meyer, 2018.<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6315968/" target="_blank">“Anthropomorphic breast
           and head phantoms for microwave imaging"</a>. Diagnostics, 2018, 8, 4, p. 85, doi : 10.3390/
           diagnostics 8040085</li>
-    </ul>
+    </ol>
     </div>
 
   <footer>
